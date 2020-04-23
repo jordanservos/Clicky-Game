c460cff305d228bafd6936b44fb0ebbac27d39c0
Jordan Servos <jservos11@yahoo.com>
2020-04-22 22:44:19 -0600

    Updates

---
+++ precache-manifest.b07601165f20778c4bd087b6d1eb4e3d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7775fbf6610aac3cbfd802c9407d0332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1795a094c89f9a69ca6",
+    "url": "/static/css/main.5c72952d.chunk.css"
+  },
+  {
+    "revision": "d66c202239228cbeba5a",
+    "url": "/static/js/2.52dc2ce1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.52dc2ce1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1795a094c89f9a69ca6",
+    "url": "/static/js/main.002bc492.chunk.js"
+  },
+  {
+    "revision": "f5664a555cfefab31111",
+    "url": "/static/js/runtime-main.c2397312.js"
+  },
+  {
+    "revision": "4b98c91491288a882582d540daec7f59",
+    "url": "/static/media/Eevee.4b98c914.png"
+  },
+  {
+    "revision": "667a99cd3922b29f5ecdf58597ad62f3",
+    "url": "/static/media/ash.667a99cd.png"
+  },
+  {
+    "revision": "3b7c03da333830288b0086f1a0e0498b",
+    "url": "/static/media/bulba.3b7c03da.png"
+  },
+  {
+    "revision": "c8f0acd853b45fa6f9ce0b14fd68ebab",
+    "url": "/static/media/charizard.c8f0acd8.png"
+  },
+  {
+    "revision": "9c6362bc623ad1a18295e284b6e36225",
+    "url": "/static/media/chikorita.9c6362bc.png"
+  },
+  {
+    "revision": "5727e0a4dd45837f5bdcac945250c89e",
+    "url": "/static/media/fox.5727e0a4.png"
+  },
+  {
+    "revision": "1a48299040330ae278094279a150d5d2",
+    "url": "/static/media/froggy.1a482990.png"
+  },
+  {
+    "revision": "3b469ded83fbc73da9df750691443ad3",
+    "url": "/static/media/meowth.3b469ded.png"
+  },
+  {
+    "revision": "2a71cf29c2a57c222498047a79f32914",
+    "url": "/static/media/mew.2a71cf29.png"
+  },
+  {
+    "revision": "14650594c58277fc7c3a428003207130",
+    "url": "/static/media/mewtwo.14650594.png"
+  },
+  {
+    "revision": "5df4c2cda01a0ede1b5a4906bdb208c9",
+    "url": "/static/media/monkey.5df4c2cd.png"
+  },
+  {
+    "revision": "c72d6d77bad74905e52160388bca8381",
+    "url": "/static/media/pikachu.c72d6d77.png"
+  },
+  {
+    "revision": "4e18784d92f021d3d0119e6015bd8996",
+    "url": "/static/media/pwall.4e18784d.jpg"
+  },
+  {
+    "revision": "6d099121a5e51774998f84a88074ebfd",
+    "url": "/static/media/simisear.6d099121.png"
+  },
+  {
+    "revision": "aad1908d8d30cdd5144cce92d9a9364c",
+    "url": "/static/media/squirtle.aad1908d.png"
+  },
+  {
+    "revision": "01174de98bc1dea57b5ff15f25895281",
+    "url": "/static/media/vapareon.01174de9.png"
+  }
+]);
